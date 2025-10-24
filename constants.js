5a32d3013ecc5f6e5c199286cd85bc8a7deec706
Domenico Ciavattone <116182960+domenicoc993@users.noreply.github.com>
2025-10-24 17:25:30 +0200

    Add new SVG icons and update constants
    Added new SVG icons for filter, change file, and export functionalities. Updated category order and utility colors to include 'Computer'.

--- constants.js
+++ constants.js
@@ -1,22 +1,50 @@
 export const svgIconCollapse = `
-<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffffff" stroke-width="0.4800000000000001" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: var(--darkreader-text-000000, #e8e6e3);"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M2.25 6C2.25 5.58579 2.58579 5.25 3 5.25H21C21.4142 5.25 21.75 5.58579 21.75 6C21.75 6.41421 21.4142 6.75 21 6.75H3C2.58579 6.75 2.25 6.41421 2.25 6ZM2.25 10C2.25 9.58579 2.58579 9.25 3 9.25H21C21.4142 9.25 21.75 9.58579 21.75 10C21.75 10.4142 21.4142 10.75 21 10.75H3C2.58579 10.75 2.25 10.4142 2.25 10ZM14.4697 12.9697C14.7626 12.6768 15.2374 12.6768 15.5303 12.9697L17.5 14.9393L19.4697 12.9697C19.7626 12.6768 20.2374 12.6768 20.5303 12.9697C20.8232 13.2626 20.8232 13.7374 20.5303 14.0303L18.5607 16L20.5303 17.9697C20.8232 18.2626 20.8232 18.7374 20.5303 19.0303C20.2374 19.3232 19.7626 19.3232 19.4697 19.0303L17.5 17.0607L15.5303 19.0303C15.2374 19.3232 14.7626 19.3232 14.4697 19.0303C14.1768 18.7374 14.1768 18.2626 14.4697 17.9697L16.4393 16L14.4697 14.0303C14.1768 13.7374 14.1768 13.2626 14.4697 12.9697ZM2.25 14C2.25 13.5858 2.58579 13.25 3 13.25H11C11.4142 13.25 11.75 13.5858 11.75 14C11.75 14.4142 11.4142 14.75 11 14.75H3C2.58579 14.75 2.25 14.4142 2.25 14ZM2.25 18C2.25 17.5858 2.58579 17.25 3 17.25H11C11.4142 17.25 11.75 17.5858 11.75 18C11.75 18.4142 11.4142 18.75 11 18.75H3C2.58579 18.75 2.25 18.4142 2.25 18Z" fill="#ffffffff" data-darkreader-inline-fill="" style="--darkreader-inline-fill: var(--darkreader-background-1c274c, #ffffffff);"></path> </g></svg>
+<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+<line x1="5.12006" y1="5.83754" x2="20.8799" y2="5.83754" stroke="#202020" stroke-width="2" stroke-linecap="round"/>
+<line x1="5.12006" y1="20.0825" x2="12" y2="20.0825" stroke="#202020" stroke-width="2" stroke-linecap="round"/>
+<line x1="5.12006" y1="15.6425" x2="12" y2="15.6425" stroke="#202020" stroke-width="2" stroke-linecap="round"/>
+<line x1="5.12006" y1="10.6475" x2="20.8799" y2="10.6475" stroke="#202020" stroke-width="2" stroke-linecap="round"/>
+<path d="M15.8377 15.4802L20.8843 20.4109" stroke="#202020" stroke-width="2" stroke-linecap="round"/>
+<path d="M20.8434 15.3234L15.7968 20.2542" stroke="#202020" stroke-width="2" stroke-linecap="round"/>
+</svg>
+
 `;
 export const svgIconExpand = `
 <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffffff" stroke-width="0.4800000000000001" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: var(--darkreader-text-000000, #e8e6e3);"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M2.25 6C2.25 5.58579 2.58579 5.25 3 5.25H21C21.4142 5.25 21.75 5.58579 21.75 6C21.75 6.41421 21.4142 6.75 21 6.75H3C2.58579 6.75 2.25 6.41421 2.25 6ZM2.25 10C2.25 9.58579 2.58579 9.25 3 9.25H21C21.4142 9.25 21.75 9.58579 21.75 10C21.75 10.4142 21.4142 10.75 21 10.75H3C2.58579 10.75 2.25 10.4142 2.25 10ZM14.4697 12.9697C14.7626 12.6768 15.2374 12.6768 15.5303 12.9697L17.5 14.9393L19.4697 12.9697C19.7626 12.6768 20.2374 12.6768 20.5303 12.9697C20.8232 13.2626 20.8232 13.7374 20.5303 14.0303L18.5607 16L20.5303 17.9697C20.8232 18.2626 20.8232 18.7374 20.5303 19.0303C20.2374 19.3232 19.7626 19.3232 19.4697 19.0303L17.5 17.0607L15.5303 19.0303C15.2374 19.3232 14.7626 19.3232 14.4697 19.0303C14.1768 18.7374 14.1768 18.2626 14.4697 17.9697L16.4393 16L14.4697 14.0303C14.1768 13.7374 14.1768 13.2626 14.4697 12.9697ZM2.25 14C2.25 13.5858 2.58579 13.25 3 13.25H11C11.4142 13.25 11.75 13.5858 11.75 14C11.75 14.4142 11.4142 14.75 11 14.75H3C2.58579 14.75 2.25 14.4142 2.25 14ZM2.25 18C2.25 17.5858 2.58579 17.25 3 17.25H11C11.4142 17.25 11.75 17.5858 11.75 18C11.75 18.4142 11.4142 18.75 11 18.75H3C2.58579 18.75 2.25 18.4142 2.25 18Z" fill="#ffffffff" data-darkreader-inline-fill="" style="--darkreader-inline-fill: var(--darkreader-background-1c274c, #ffffffff);"></path> </g></svg>
 `;
 export const svgIconHistory = `
-<svg viewBox="-0.96 -0.96 25.92 25.92" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff" stroke-width="0.336" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: var(--darkreader-text-ffffff, #e8e6e3);"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3 5.67541V3C3 2.44772 2.55228 2 2 2C1.44772 2 1 2.44772 1 3V7C1 8.10457 1.89543 9 3 9H7C7.55229 9 8 8.55229 8 8C8 7.44772 7.55229 7 7 7H4.52186C4.54218 6.97505 4.56157 6.94914 4.57995 6.92229C5.621 5.40094 7.11009 4.22911 8.85191 3.57803C10.9074 2.80968 13.173 2.8196 15.2217 3.6059C17.2704 4.3922 18.9608 5.90061 19.9745 7.8469C20.9881 9.79319 21.2549 12.043 20.7247 14.1724C20.1945 16.3018 18.9039 18.1638 17.0959 19.4075C15.288 20.6513 13.0876 21.1909 10.9094 20.9247C8.73119 20.6586 6.72551 19.605 5.27028 17.9625C4.03713 16.5706 3.27139 14.8374 3.06527 13.0055C3.00352 12.4566 2.55674 12.0079 2.00446 12.0084C1.45217 12.0088 0.995668 12.4579 1.04626 13.0078C1.25994 15.3309 2.2082 17.5356 3.76666 19.2946C5.54703 21.3041 8.00084 22.5931 10.6657 22.9188C13.3306 23.2444 16.0226 22.5842 18.2345 21.0626C20.4464 19.541 22.0254 17.263 22.6741 14.6578C23.3228 12.0526 22.9963 9.30013 21.7562 6.91897C20.5161 4.53782 18.448 2.69239 15.9415 1.73041C13.4351 0.768419 10.6633 0.756291 8.14853 1.69631C6.06062 2.47676 4.26953 3.86881 3 5.67541Z" fill="#ffffff" data-darkreader-inline-fill="" style="--darkreader-inline-fill: var(--darkreader-background-ffffff, #ffffffff);"></path> <path d="M12 5C11.4477 5 11 5.44771 11 6V12.4667C11 12.4667 11 12.7274 11.1267 12.9235C11.2115 13.0898 11.3437 13.2344 11.5174 13.3346L16.1372 16.0019C16.6155 16.278 17.2271 16.1141 17.5032 15.6358C17.7793 15.1575 17.6155 14.546 17.1372 14.2698L13 11.8812V6C13 5.44772 12.5523 5 12 5Z" fill="#ffffff" data-darkreader-inline-fill="" style="--darkreader-inline-fill: var(--darkreader-background-ffffff, #ffffffff);"></path> </g></svg>
+<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M5.88064 7.99699V5.88064C5.88064 5.44376 5.52647 5.0896 5.0896 5.0896C4.65272 5.0896 4.29856 5.44376 4.29856 5.88064V9.04479C4.29856 9.91855 5.00688 10.6269 5.88064 10.6269H9.04479C9.48167 10.6269 9.83583 10.2727 9.83583 9.83583C9.83583 9.39896 9.48167 9.04479 9.04479 9.04479H7.08449C7.10056 9.02506 7.1159 9.00456 7.13044 8.98332C7.95395 7.77987 9.13188 6.85291 10.5097 6.33788C12.1357 5.73008 13.9279 5.73793 15.5485 6.35993C17.1691 6.98192 18.5062 8.17513 19.3081 9.71472C20.1099 11.2543 20.321 13.034 19.9016 14.7184C19.4822 16.4029 18.4612 17.8758 17.031 18.8596C15.6009 19.8435 13.8603 20.2704 12.1373 20.0598C10.4142 19.8493 8.82766 19.0158 7.67651 17.7166C6.70105 16.6155 6.09532 15.2445 5.93227 13.7954C5.88342 13.3612 5.53 13.0062 5.09312 13.0066C4.65624 13.007 4.29513 13.3622 4.33515 13.7972C4.50418 15.6349 5.25429 17.3789 6.48709 18.7703C7.89543 20.3599 9.83649 21.3795 11.9445 21.6372C14.0525 21.8948 16.182 21.3725 17.9317 20.1689C19.6814 18.9652 20.9305 17.1632 21.4436 15.1024C21.9568 13.0416 21.6985 10.8643 20.7175 8.98069C19.7366 7.09711 18.1006 5.6373 16.1179 4.87634C14.1352 4.11537 11.9426 4.10577 9.95332 4.84937C8.3017 5.46673 6.88488 6.5679 5.88064 7.99699Z" fill="#202020" stroke="#202020" stroke-width="0.7"/>
+<path d="M12.7677 9.41705C12.427 9.41705 12.1508 9.66399 12.1508 9.96861V13.5354C12.1508 13.5354 12.1508 13.6792 12.229 13.7874C12.2813 13.8791 12.3628 13.9588 12.47 14.0141L15.3201 15.4853C15.6152 15.6376 15.9925 15.5472 16.1629 15.2834C16.3332 15.0195 16.2321 14.6823 15.9371 14.5299L13.3847 13.2125V9.96861C13.3847 9.664 13.1085 9.41705 12.7677 9.41705Z" fill="#202020" stroke="#202020"/>
+</svg>
+
+
 `;
 export const svgIconExport = `
-<svg height="64px" width="64px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 332.904 332.904" xml:space="preserve" fill="#ffffffff" data-darkreader-inline-fill="" style="--darkreader-inline-fill: var(--darkreader-background-000000, #ffffffff); --darkreader-inline-stroke: var(--darkreader-text-000000, #e8e6e3);" stroke="#ffffffff" stroke-width="31.292976" data-darkreader-inline-stroke=""><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path style="fill: rgba(255, 255, 255, 1); --darkreader-inline-fill: var(--darkreader-text-010002, #e7e5e2);" d="M248.001,220.774c-17.483,0-33.116,8.055-43.403,20.639l-75.761-44.232 c7.572-9.559,12.131-21.606,12.131-34.715c0-12.154-3.926-23.378-10.526-32.573l72.265-40.945 c10.197,14.01,26.678,23.169,45.295,23.169c30.908,0,56.059-25.144,56.059-56.059S278.909,0,248.001,0 s-56.059,25.144-56.059,56.059c0,8.025,1.724,15.651,4.779,22.561l-74.449,42.186c-9.929-8.915-23.002-14.392-37.371-14.392 c-30.908,0-56.059,25.144-56.059,56.059c0,30.908,25.144,56.059,56.059,56.059c13.372,0,25.646-4.72,35.294-12.56l77.892,45.474 c-3.896,7.632-6.146,16.254-6.146,25.401c0,30.908,25.15,56.059,56.059,56.059s56.059-25.15,56.059-56.059 S278.909,220.774,248.001,220.774z" data-darkreader-inline-fill=""></path> </g> </g></svg>
+<svg width="26" height="26" viewBox="0 0 26 26" fill="#202020" xmlns="http://www.w3.org/2000/svg">
+<circle cx="8.70421" cy="12.948" r="2.48039" transform="rotate(-37.254 8.70421 12.948)" fill="#202020" stroke="#202020"/>
+<circle cx="16.6118" cy="6.9341" r="2.48039" transform="rotate(-37.254 16.6118 6.9341)" fill="#202020" stroke="#202020"/>
+<line x1="8.70025" y1="12.9428" x2="17.3986" y2="6.3275" stroke="#202020" stroke-width="2"/>
+<circle cx="8.70893" cy="12.8996" r="2.48039" transform="rotate(37.746 8.70893 12.8996)" fill="#202020" stroke="#202020"/>
+<circle cx="16.5646" cy="18.9812" r="2.48039" transform="rotate(37.746 16.5646 18.9812)" fill="#202020" stroke="#202020"/>
+<line x1="8.71294" y1="12.8944" x2="17.3541" y2="19.5842" stroke="#202020" stroke-width="2"/>
+</svg>
+
 `;
 
 export const svgIconFilter = `
-<svg viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M17.8258 5H6.17422C5.31987 5 4.85896 6.00212 5.41496 6.65079L9.75926 11.7191C9.91461 11.9004 10 12.1312 10 12.3699V17.382C10 17.7607 10.214 18.107 10.5528 18.2764L12.5528 19.2764C13.2177 19.6088 14 19.1253 14 18.382V12.3699C14 12.1312 14.0854 11.9004 14.2407 11.7191L18.585 6.65079C19.141 6.00212 18.6801 5 17.8258 5Z" stroke="#ffffffff" stroke-width="2.04" stroke-linecap="round" stroke-linejoin="round" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: var(--darkreader-text-000000, #e8e6e3);"></path> </g></svg>
+<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M18.9728 5H7.02722C6.15131 5 5.67876 6.11476 6.2488 6.83635L10.7027 12.4744C10.862 12.676 10.9495 12.9328 10.9495 13.1983V18.7738C10.9495 19.1951 11.1689 19.5803 11.5163 19.7687L13.5668 20.8812C14.2484 21.2509 15.0505 20.7131 15.0505 19.8862V13.1983C15.0505 12.9328 15.138 12.676 15.2973 12.4744L19.7512 6.83635C20.3212 6.11476 19.8487 5 18.9728 5Z" stroke="#202020" stroke-width="2.04" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
 `;
 
 export const svgIconChangeFile = `
-<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M13.5 3H12H7C5.89543 3 5 3.89543 5 5V19C5 20.1046 5.89543 21 7 21H7.5M13.5 3L19 8.625M13.5 3V7.625C13.5 8.17728 13.9477 8.625 14.5 8.625H19M19 8.625V9.75V12V19C19 20.1046 18.1046 21 17 21H16.5" stroke="#ffffffff" stroke-width="2.04" stroke-linecap="round" stroke-linejoin="round" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: var(--darkreader-text-000000, #e8e6e3);"></path> <path d="M12 21L12 13M12 13L14.5 15.5M12 13L9.5 15.5" stroke="#ffffffff" stroke-width="2.04" stroke-linecap="round" stroke-linejoin="round" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: var(--darkreader-text-000000, #e8e6e3);"></path> </g></svg>
+<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M14.2866 4.99449H13H8.71134C7.76391 4.99449 6.99587 5.79098 6.99587 6.77349V19.2265C6.99587 20.209 7.76391 21.0055 8.71134 21.0055H9.1402M14.2866 4.99449L19.0041 9.99794M14.2866 4.99449V9.10843C14.2866 9.59969 14.6706 9.99794 15.1443 9.99794H19.0041M19.0041 9.99794V10.9986V13V19.2265C19.0041 20.209 18.2361 21.0055 17.2887 21.0055H16.8598" stroke="#202020" stroke-width="2.04" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M13 21.0055V13.8895M13 13.8895L15.1443 16.1133M13 13.8895L10.8557 16.1133" stroke="#202020" stroke-width="2.04" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
 `;
 
 export const CATEGORY_ORDER = [
@@ -30,6 +58,7 @@ export const CATEGORY_ORDER = [
   "Luci",
   "Borse",
   "Altro",
+  "Computer",
 ];
 
 export const UTILIZZO_COLORS = {
@@ -49,5 +78,6 @@ export const CATEGORY_COLORS = {
   Luci: "#dc4c9166",
   Borse: "#de555373",
   Altro: "#ffffff18",
+  Computer: "#ff80596D",
   Default: "#3c5354ff",
 };
